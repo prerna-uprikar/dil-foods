17c299be600dda70040a294fb23cc0a6cc777b74
prerna-uprikar <128795299+prerna-uprikar@users.noreply.github.com>
2023-12-18 02:02:12 +0530

    Revert "components created"
    This reverts commit 4a6ac7fe4810619bde60f87836d214240af49ac7.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import Header from './components/header/Header'
 
 export default function Home() {
   return (
     <>
-     <Header />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
