6346b18d01e57ba370762fc65c91706ad6fe6536
prerna-uprikar <128795299+prerna-uprikar@users.noreply.github.com>
2023-12-18 02:38:41 +0530

    Header added

---
+++ src/app/components/header/Header.js
@@ -0,0 +1,19 @@
+import CartIcon from "@/assets/Cart";
+import styles from "./Header.module.scss";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className={` ${styles.headerSection} flex items-center justify-between py-[10px] px-[40px]`}>
+            <Image src="https://dilfoods.in/wp-content/uploads/2023/04/Dil-Foods-new-logo.png" width={70} height={60} />
+          <button className="bg-[#e30217] text-white px-[30px] py-[10px] flex gap-[10px] rounded-[6px]">
+            Cart
+          <CartIcon />
+          </button>
+        </div>
+      </header>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Image from 'next/image'
+import Header from "./components/header/Header";
+
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <Header />
     </>
   )
 }
