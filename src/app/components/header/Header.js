982c72f2556babea3d249d77aa37bc93a7d6745c
prerna-uprikar <128795299+prerna-uprikar@users.noreply.github.com>
2023-12-18 02:51:38 +0530

    Hero section added

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,19 +1,27 @@
+import React from "react";
 import CartIcon from "@/assets/Cart";
 import styles from "./Header.module.scss";
 import Image from "next/image";
 
 export default function Header() {
   return (
-    <>
-      <header>
-        <div className={` ${styles.headerSection} flex items-center justify-between py-[10px] px-[40px]`}>
-            <Image src="https://dilfoods.in/wp-content/uploads/2023/04/Dil-Foods-new-logo.png" width={70} height={60} />
-          <button className="bg-[#e30217] text-white px-[30px] py-[10px] flex gap-[10px] rounded-[6px]">
-            Cart
+    <header>
+      <div
+        className={` ${styles.headerSection} flex items-center justify-between py-[10px] px-[40px]`}
+      >
+        <Image
+          src="https://dilfoods.in/wp-content/uploads/2023/04/Dil-Foods-new-logo.png"
+          width={70}
+          height={60}
+        />
+        <button
+          title="Cart"
+          className="bg-[#e30217] text-white px-[30px] py-[10px] flex gap-[10px] rounded-[6px]"
+        >
+          Cart
           <CartIcon />
-          </button>
-        </div>
-      </header>
-    </>
+        </button>
+      </div>
+    </header>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Header from "./components/header/Header";
-
+import Hero from "./components/heroSection/Hero";
 
 export default function Home() {
   return (
     <>
-   <Header />
+      <Header />
+      <Hero />
     </>
-  )
+  );
 }
