4a6ac7fe4810619bde60f87836d214240af49ac7
prerna-uprikar <128795299+prerna-uprikar@users.noreply.github.com>
2023-12-18 01:52:18 +0530

    components created

---
+++ src/app/components/header/Header.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <Header>
+      <div className=''>
+          <p>Dil</p>
+          <p>Foods</p>
+      </div>
+    </Header>
+  )
+}
