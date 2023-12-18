27506efd5524feec30dba74fc24684cbd92210c7
prerna-uprikar <128795299+prerna-uprikar@users.noreply.github.com>
2023-12-19 01:19:14 +0530

    Json data for items added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['dilfoods.in', 'luna-meraki.svc.urbanpiper.com', 'cdn.urbanpiper.com'],
+        domains: ['dilfoods.in', 'luna-meraki.svc.urbanpiper.com', 'cdn.urbanpiper.com', 'your-image-url.com'],
       },    
 }
 
--- src/app/components/productSection/ProductSection.js
+++ src/app/components/productSection/ProductSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./ProductSection.module.scss";
+import itemsData from "../JSON/items.json"
 
 export default function ProductSection() {
   return (
@@ -9,131 +10,25 @@ export default function ProductSection() {
         Our Products
       </h1>
       <div className="flex flex-wrap justify-center gap-[60px]">
-        <div className={`${styles.productCard}`}>
-          <Image
-            src="https://luna-meraki.svc.urbanpiper.com/_next/image?url=https%3A%2F%2Fcdn.urbanpiper.com%2Fmedia%2Fbizmedia%2F2023%2F10%2F30%2FQrWqf-OwI8S6A0GpeI0yap4PhR65rAmrvhga17AFUUt1cT.jpg&q=80&w=320"
-            height="200"
-            width="300"
-            alt="product image"
-          />
-          <h2 className="text-center pt-[10px] text-[18px] font-[500]">
-            Garlic Bread
-          </h2>
-          <div className="flex justify-between p-[20px]">
-            <span className="text-green">Rs.40</span>
-            <span className="border border-[#e30217] text-[#e30217] px-[10px] cursor-pointer rounded-[6px]">
-              Add
-            </span>
+        {itemsData.map((item) => (
+          <div key={item.id} className={`${styles.productCard} cursor-pointer`}>
+            <Image
+              src={item.image}
+              height={300}
+              width={300}
+              alt={item.name}
+            />
+            <h2 className="text-center pt-[10px] text-[18px] font-[500]">
+              {item.name}
+            </h2>
+            <div className="flex justify-between p-[20px]">
+              <span className="text-green">Rs. {item.price}</span>
+              <span className="border border-[#e30217] text-[#e30217] px-[10px] cursor-pointer rounded-[6px]">
+                Add
+              </span>
+            </div>
           </div>
-        </div>
-
-        <div className={`${styles.productCard}`}>
-          <Image
-            src="https://luna-meraki.svc.urbanpiper.com/_next/image?url=https%3A%2F%2Fcdn.urbanpiper.com%2Fmedia%2Fbizmedia%2F2023%2F10%2F30%2FkIuGWV-0uD0ME3stV4zVdayrUkS84iR4H9pednOqFsRjeRF.jpg&q=80&w=320"
-            height="300"
-            width="300"
-            alt="product image"
-          />
-          <h2 className="text-center pt-[10px] text-[18px] font-[500]">
-            Chicken Grilled Salad
-          </h2>
-          <div className="flex justify-between p-[20px]">
-            <span className="text-green">Rs.40</span>
-            <span className="border border-[#e30217] text-[#e30217] px-[10px] cursor-pointer rounded-[6px]">
-              Add
-            </span>
-          </div>
-        </div>
-
-        <div className={`${styles.productCard}`}>
-          <Image
-            src="https://luna-meraki.svc.urbanpiper.com/_next/image?url=https%3A%2F%2Fcdn.urbanpiper.com%2Fmedia%2Fbizmedia%2F2023%2F10%2F30%2FW2ky5Kb-EA2RDAPxreEQsvRAKCCfCPQI86A6NLTLC0juF8IZ.jpg&q=80&w=320"
-            height="300"
-            width="300"
-            alt="product image"
-          />
-          <h2 className="text-center pt-[10px] text-[18px] font-[500]">
-            Raspberry Cheesecake
-          </h2>
-          <div className="flex justify-between p-[20px]">
-            <span className="text-green">Rs.40</span>
-            <span className="border border-[#e30217] text-[#e30217] px-[10px] cursor-pointer rounded-[6px]">
-              Add
-            </span>
-          </div>
-        </div>
-
-        <div className={`${styles.productCard}`}>
-          <Image
-            src="https://luna-meraki.svc.urbanpiper.com/_next/image?url=https%3A%2F%2Fcdn.urbanpiper.com%2Fmedia%2Fbizmedia%2F2023%2F10%2F30%2FOHWAf-bcdD64Jv6PzgV5T6PckEC0MQ9U09fYVMF7xB3Jsu.jpg&q=80&w=320"
-            height="300"
-            width="300"
-            alt="product image"
-          />
-          <h2 className="text-center pt-[10px] text-[18px] font-[500]">
-            Creamy Spinach
-          </h2>
-          <div className="flex justify-between p-[20px]">
-            <span className="text-green">Rs.40</span>
-            <span className="border border-[#e30217] text-[#e30217] px-[10px] cursor-pointer rounded-[6px]">
-              Add
-            </span>
-          </div>
-        </div>
-
-        <div className={`${styles.productCard}`}>
-          <Image
-            src="https://luna-meraki.svc.urbanpiper.com/_next/image?url=https%3A%2F%2Fcdn.urbanpiper.com%2Fmedia%2Fbizmedia%2F2023%2F10%2F30%2FQd0h1a-QAV1RaaebAZZdNrcAhemyQ8K520IzmU2PpuxB6pG.jpg&q=80&w=320"
-            height="300"
-            width="300"
-            alt="product image"
-          />
-          <h2 className="text-center pt-[10px] text-[18px] font-[500]">
-            Veg Salad
-          </h2>
-          <div className="flex justify-between p-[20px]">
-            <span className="text-green">Rs.40</span>
-            <span className="border border-[#e30217] text-[#e30217] px-[10px] cursor-pointer rounded-[6px]">
-              Add
-            </span>
-          </div>
-        </div>
-
-        <div className={`${styles.productCard}`}>
-          <Image
-            src="https://luna-meraki.svc.urbanpiper.com/_next/image?url=https%3A%2F%2Fcdn.urbanpiper.com%2Fmedia%2Fbizmedia%2F2023%2F10%2F30%2FBU2TZUXG-iSSNOLKIVPny6oeMeCcnOcELiVjGLD6obpnwxfRe.jpg&q=80&w=320"
-            height="300"
-            width="300"
-            alt="product image"
-          />
-          <h2 className="text-center pt-[10px] text-[18px] font-[500]">
-            Penne in Cream Sauce
-          </h2>
-          <div className="flex justify-between p-[20px]">
-            <span className="text-green">Rs.40</span>
-            <span className="border border-[#e30217] text-[#e30217] px-[10px] cursor-pointer rounded-[6px]">
-              Add
-            </span>
-          </div>
-        </div>
-
-        <div className={`${styles.productCard}`}>
-          <Image
-            src="https://luna-meraki.svc.urbanpiper.com/_next/image?url=https%3A%2F%2Fcdn.urbanpiper.com%2Fmedia%2Fbizmedia%2F2023%2F10%2F30%2Fjzwguk-pe8g2rrQkhTzgtTJcLopIc78yYZoXIVHpKRo1l3D.jpg&q=80&w=320"
-            height="300"
-            width="300"
-            alt="product image"
-          />
-          <h2 className="text-center pt-[10px] text-[18px] font-[500]">
-            Dough Balls Doppio
-          </h2>
-          <div className="flex justify-between p-[20px]">
-            <span className="text-green">Rs.40</span>
-            <span className="border border-[#e30217] text-[#e30217] px-[10px] cursor-pointer rounded-[6px]">
-              Add
-            </span>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
